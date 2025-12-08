0c46afc8ff968016d5cbd96d5d36e6b503eae97d
Tori Champagne <24481033@studentmail.ul.ie>
2025-12-07 22:48:58 -0500

    some updates-- getting rid of zip files

--- src/navigation.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-//this file will handle going in and out the cottage
-//this will also handle moving the player around
-//this may handle the actions of the player like opening the door, or clicking on objects (might put in respective files)
-//this will also handle the interactions with the environment, like the trees, the grass, the rocks, the water, etc.
-//like there should be a dif stepping sound when walk outside on snow and inside on floor
-//will also include the animation loop for the player
-//it is incredibly important that we break down EVERYTHING into as many smaller functions as possible
